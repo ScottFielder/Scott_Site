0273b19e9fd87abecef88b50d26e160b58ebea99
Scott Fielder <fielder@humber.ca>
2025-01-31 22:45:03 -0500

    Update 258

--- GAME258-Engines4/MaterialComponent.cpp
+++
@@ -1,9 +0,0 @@
-#include "MaterialComponent.h"
-MaterialComponent::MaterialComponent(Component* parent_):Component(parent_) {}
-MaterialComponent::~MaterialComponent() {}
-bool MaterialComponent::OnCreate() {
-	return true;
-}
-void MaterialComponent::OnDestroy() {}
-void MaterialComponent::Update(const float deltaTime) {}
-void MaterialComponent::Render()const {}
